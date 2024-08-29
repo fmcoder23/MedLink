aaf95cbdd93f09cb14a1488b035e1ccb430b282b
Muhammadjon <tokhirov.mukhammadjon@gmail.com>
2024-08-29 11:07:33 +0500

    format-response in review

--- backend/src/module/review/review.service.ts
+++ backend/src/module/review/review.service.ts
@@ -2,19 +2,22 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { formatResponse } from 'src/common/utils/response.util';
 
 @Injectable()
 export class ReviewService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createReviewDto: CreateReviewDto) {
-    return this.prisma.review.create({
+    const review = await this.prisma.review.create({
       data: createReviewDto,
     });
+    return formatResponse('Review created successfully', review);
   }
 
   async findAll() {
-    return this.prisma.review.findMany();
+    const reviews = await this.prisma.review.findMany();
+    return formatResponse('All reviews retrieved successfully', reviews);
   }
 
   async findOne(id: string) {
@@ -24,21 +27,23 @@ export class ReviewService {
     if (!review) {
       throw new NotFoundException(`Review with ID ${id} not found`);
     }
-    return review;
+    return formatResponse('Review retrieved successfully', review);
   }
 
   async update(id: string, updateReviewDto: UpdateReviewDto) {
     const { doctorId, patientId, ...rest } = updateReviewDto;
 
-    return this.prisma.review.update({
+    const updatedReview = await this.prisma.review.update({
       where: { id },
       data: rest,
     });
+    return formatResponse('Review updated successfully', updatedReview);
   }
 
   async remove(id: string) {
-    return this.prisma.review.delete({
+    await this.prisma.review.delete({
       where: { id },
     });
+    return formatResponse('Review deleted successfully', null);
   }
 }
